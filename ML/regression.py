7febf2b7ea0f2093e3b38ecbe740f31583e96492
Karol <physalium2@gmail.com>
2020-04-12 23:15:43 +0200

    Pierwszy commit

---
+++ ML/regression.py
@@ -0,0 +1,37 @@
+import quandl, math
+import numpy as np
+import pandas as pd
+from sklearn import preprocessing, svm, model_selection
+from sklearn.linear_model import LinearRegression
+
+df = quandl.get("WIKI/GOOGL")
+print(df.head())
+# print(df.tail())
+
+df = df[["Adj. Open", "Adj. High", "Adj. Low", "Adj. Close", "Adj. Volume"]]
+
+df["HL_PCT"] = (df["Adj. High"] - df["Adj. Low"]) / df["Adj. Close"] * 100.0
+df["PCT_change"] = (df["Adj. Close"] - df["Adj. Open"]) / df["Adj. Open"] * 100.0
+
+df = df[["Adj. Close", "HL_PCT", "PCT_change", "Adj. Volume"]]
+print(df.head())
+
+forecast_col = "Adj. Close"
+df.fillna(value=-99999, inplace=True)
+forecast_out = int(math.ceil(0.01 * len(df)))
+
+df["label"] = df[forecast_col].shift(-forecast_out)
+
+df.dropna(inplace=True)
+X = np.array(df.drop(["label"], 1))
+y = np.array(df["label"])
+
+X = preprocessing.scale(X)
+y = np.array(df["label"])
+X_train, X_test, y_train, y_test = model_selection.train_test_split(
+    X, y, test_size=0.2
+)
+clf = svm.SVR()
+clf.fit(X_train, y_train)
+confidence = clf.score(X_test, y_test)
+clf = LinearRegression()
